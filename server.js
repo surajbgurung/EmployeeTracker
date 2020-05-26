845c73aa2431c34ee32dea2694505646ced56183
surajgurung <microbiosuraj@gmail.com>
2020-05-26 17:41:23 -0400

    making budget

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ function start() {
                 "Add Roles",
                 "Remove Employee",
                 "Update Employee",
+                "Total Budget By DepartMent",
                 "Exit"
             ]
 
@@ -50,7 +51,7 @@ function start() {
                 case "View all Department and its Employee":
                     employeeDepartment();
                     break;
-                    case "View all Employee with its Department and Salary":
+                case "View all Employee with its Department and Salary":
                     employeeDepartmentAll();
                     break;
                 case "View all Employeee By Manager":
@@ -70,6 +71,9 @@ function start() {
                     break;
                 case "Update Employee":
                     updateEmployee();
+                    break;
+                case "Total Budget By DepartMent":
+                    budgetTotal();
                     break;
                 case "Exit":
                     connection.end();
@@ -133,17 +137,17 @@ function employeeDepartment() {
 
 }
 //to view employee with depatment and salary************************************** EMPLOYEE WITH DEPARTMENT AND SALARY *******************************************
-function employeeDepartmentAll(){
+function employeeDepartmentAll() {
     var query = `SELECT employee.first_name as "First Name", employee.last_name as "Last Name",department.name as "Department",role.salary as "Salary/Week" FROM employee
                 JOIN role
                 ON employee.role_id=role.id
                 JOIN department
                 ON role.department_id=department.id;`
-                connection.query(query, function (err, res) {
-                    if (err) throw err;
-                    console.table(res);
-                    start();
-                });
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
 
 }
 
@@ -514,177 +518,191 @@ function employeeAll() {
 
 
 
-    ///update employee ************************************** UPDATE EMPLOYEE *******************************************
-    function updateEmployee() {
-        inquirer
-            .prompt({
-                name: "update",
-                type: "list",
-                message: "Update Employee By",
-                choices: [
-                    "First Name",
-                    "Last Name",
-                    "Update Role"
+///update employee ************************************** UPDATE EMPLOYEE *******************************************
+function updateEmployee() {
+    inquirer
+        .prompt({
+            name: "update",
+            type: "list",
+            message: "Update Employee By",
+            choices: [
+                "First Name",
+                "Last Name",
+                "Update Role"
 
-                ]
+            ]
 
-            }).then(function (answer) {
-                switch (answer.update) {
+        }).then(function (answer) {
+            switch (answer.update) {
 
-                    case "First Name":
-                        updateFirstname();
-                        break;
-                    case "Last Name":
-                        updateLastName();
-                        break;
-                    case "Update Role":
-                        updateRole();
-                        break;
+                case "First Name":
+                    updateFirstname();
+                    break;
+                case "Last Name":
+                    updateLastName();
+                    break;
+                case "Update Role":
+                    updateRole();
+                    break;
 
+            }
+        });
+
+}
+//update firstname ************************************** UPDATE FIRST NAME *******************************************
+function updateFirstname() {
+    inquirer
+        .prompt([{
+            name: "first_name",
+            type: "input",
+            message: "Update First Name",
+            validate: function validateName(name) {
+                if (name !== '') {
+                    return true;
+                } else {
+                    return "Enter The First Name";
                 }
-            });
-
-    }
-    //update firstname ************************************** UPDATE FIRST NAME *******************************************
-    function updateFirstname() {
-        inquirer
-            .prompt([{
-                name: "first_name",
-                type: "input",
-                message: "Update First Name",
-                validate: function validateName(name) {
-                    if (name !== '') {
-                        return true;
-                    } else {
-                        return "Enter The First Name";
-                    }
+            }
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Are you sure! put the 'ID' of employee",
+            validate: function (value) {
+                if (value !== '' && isNaN(value) === false) {//isNaN("suraj")===true
+                    return true;
                 }
-            },
-            {
-                name: "id",
-                type: "input",
-                message: "Are you sure! put the 'ID' of employee",
-                validate: function (value) {
-                    if (value !== '' && isNaN(value) === false) {//isNaN("suraj")===true
-                        return true;
-                    }
-                    return false;
-                }
-
-
+                return false;
             }
 
 
-            ]).then(function (answer) {
-                var query = "UPDATE employee SET? WHERE ?";
-                connection.query(query, [{ first_name: answer.first_name }, { id: answer.id }],
-                    function (err, res) {
-
-                        if (err) throw err;
-                        console.log("Given employee is Updated")
-                        console.log(res.message);
-                        start();
+        }
 
 
-                    });
+        ]).then(function (answer) {
+            var query = "UPDATE employee SET? WHERE ?";
+            connection.query(query, [{ first_name: answer.first_name }, { id: answer.id }],
+                function (err, res) {
+
+                    if (err) throw err;
+                    console.log("Given employee is Updated")
+                    console.log(res.message);
+                    start();
 
 
-
-            });
-
-
-
-    }
-
-    //update last name ************************************** UPDATE LAST NAME *******************************************
-    function updateLastName() {
-        inquirer
-            .prompt([{
-                name: "last_name",
-                type: "input",
-                message: "Update Last Name",
-                validate: function validateName(name) {
-                    if (name !== '') {
-                        return true;
-                    } else {
-                        return "Enter The Last Name";
-                    }
-                }
-            },
-            {
-                name: "id",
-                type: "input",
-                message: "Are you sure! put the 'ID' of employee",
-                validate: function (value) {
-                    if (value !== '' && isNaN(value) === false) {//isNaN("suraj")===true
-                        return true;
-                    }
-                    return false;
-                }
-
-            }
-
-            ]).then(function (answer) {
-                var query = "UPDATE employee SET? WHERE ?";
-                connection.query(query, [{ last_name: answer.last_name }, { id: answer.id }],
-                    function (err, res) {
-                        if (err) throw err;
-                        console.log(res.message);
-                        console.log("Employee is UPDATE");
-                        start();
-
-                    });
-
-
-            });
+                });
 
 
 
-    }
+        });
 
-    //update role of employee ************************************** UPDATE ROLE *******************************************
-    function updateRole() {
-        inquirer
-            .prompt([{
-                name: "role_id",
-                type: "input",
-                message: "Update Role of Employee",
-                validate: function (value) {
-                    if (value !== '' && isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
 
-            },
-            {
-                name: "id",
-                type: "input",
-                message: "Are you sure! put the 'ID' of employee",
-                validate: function (value) {
-                    if (value !== '' && isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
+
+}
+
+//update last name ************************************** UPDATE LAST NAME *******************************************
+function updateLastName() {
+    inquirer
+        .prompt([{
+            name: "last_name",
+            type: "input",
+            message: "Update Last Name",
+            validate: function validateName(name) {
+                if (name !== '') {
+                    return true;
+                } else {
+                    return "Enter The Last Name";
                 }
             }
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Are you sure! put the 'ID' of employee",
+            validate: function (value) {
+                if (value !== '' && isNaN(value) === false) {//isNaN("suraj")===true
+                    return true;
+                }
+                return false;
+            }
+
+        }
+
+        ]).then(function (answer) {
+            var query = "UPDATE employee SET? WHERE ?";
+            connection.query(query, [{ last_name: answer.last_name }, { id: answer.id }],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.message);
+                    console.log("Employee is UPDATE");
+                    start();
+
+                });
 
 
-            ]).then(function (answer) {
-                var query = "UPDATE employee SET? WHERE ?";
-                connection.query(query, [{ role_id: answer.role_id }, { id: answer.id }],
-                    function (err, res) {
-
-                        if (err) throw err;
-                        console.log("Employee Role is Updated")
-                        start();
-                    });
-
-
-            });
+        });
 
 
 
-    }
+}
+
+//update role of employee ************************************** UPDATE ROLE *******************************************
+function updateRole() {
+    inquirer
+        .prompt([{
+            name: "role_id",
+            type: "input",
+            message: "Update Role of Employee",
+            validate: function (value) {
+                if (value !== '' && isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Are you sure! put the 'ID' of employee",
+            validate: function (value) {
+                if (value !== '' && isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+        ]).then(function (answer) {
+            var query = "UPDATE employee SET? WHERE ?";
+            connection.query(query, [{ role_id: answer.role_id }, { id: answer.id }],
+                function (err, res) {
+
+                    if (err) throw err;
+                    console.log("Employee Role is Updated")
+                    start();
+                });
+
+
+        });
+
+
+
+}
+
+///total budget by department
+
+function budgetTotal() {
+    query = "SELECT d.name AS 'DepartMent Name', sum(r.salary) AS 'Total Budget', count(r.salary) AS 'Total Employees' FROM employee e, role r, department d WHERE e.role_id = r.id AND r.department_id = d.id GROUP BY r.department_id";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+       
+    })
+   
+}
+
 
 
